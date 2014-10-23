d7b8d12c38b5fedb9a3f9f7b2052a17926bf11ce
zzeraw <pekidoch@gmail.com>
2014-10-23 10:21:42 +0400

    Создание новых моделей

---
+++ protected/models/InstructorItem.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * This is the model class for table "instructor_items".
+ *
+ * The followings are the available columns in table 'instructor_items':
+ * @property integer $id
+ * @property string $fio
+ * @property string $image
+ * @property string $annotation
+ * @property string $body
+ * @property string $clubs
+ * @property string $tags
+ * @property integer $meta_index
+ * @property string $meta_title
+ * @property string $meta_keywords
+ * @property string $meta_description
+ * @property string $created_ip
+ * @property string $created_date
+ * @property integer $created_user
+ * @property string $created_username
+ * @property string $modified_ip
+ * @property string $modified_date
+ * @property integer $modified_user
+ * @property string $modified_username
+ * @property integer $active
+ *
+ * The followings are the available model relations:
+ * @property TimeboardItems[] $timeboardItems
+ */
+class InstructorItem extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'instructor_items';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('fio, image, annotation, body, created_username, modified_username', 'required'),
+			array('meta_index, created_user, modified_user, active', 'numerical', 'integerOnly'=>true),
+			array('fio, tags, meta_title, created_ip, modified_ip, modified_username', 'length', 'max'=>300),
+			array('image, created_username', 'length', 'max'=>200),
+			array('annotation, meta_keywords, meta_description', 'length', 'max'=>500),
+			array('clubs', 'length', 'max'=>100),
+			array('created_date, modified_date', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, fio, image, annotation, body, clubs, tags, meta_index, meta_title, meta_keywords, meta_description, created_ip, created_date, created_user, created_username, modified_ip, modified_date, modified_user, modified_username, active', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'timeboardItems' => array(self::HAS_MANY, 'TimeboardItems', 'instructor_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'fio' => 'Fio',
+			'image' => 'Image',
+			'annotation' => 'Annotation',
+			'body' => 'Body',
+			'clubs' => 'Clubs',
+			'tags' => 'Tags',
+			'meta_index' => 'Meta Index',
+			'meta_title' => 'Meta Title',
+			'meta_keywords' => 'Meta Keywords',
+			'meta_description' => 'Meta Description',
+			'created_ip' => 'Created Ip',
+			'created_date' => 'Created Date',
+			'created_user' => 'Created User',
+			'created_username' => 'Created Username',
+			'modified_ip' => 'Modified Ip',
+			'modified_date' => 'Modified Date',
+			'modified_user' => 'Modified User',
+			'modified_username' => 'Modified Username',
+			'active' => 'Active',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('fio',$this->fio,true);
+		$criteria->compare('image',$this->image,true);
+		$criteria->compare('annotation',$this->annotation,true);
+		$criteria->compare('body',$this->body,true);
+		$criteria->compare('clubs',$this->clubs,true);
+		$criteria->compare('tags',$this->tags,true);
+		$criteria->compare('meta_index',$this->meta_index);
+		$criteria->compare('meta_title',$this->meta_title,true);
+		$criteria->compare('meta_keywords',$this->meta_keywords,true);
+		$criteria->compare('meta_description',$this->meta_description,true);
+		$criteria->compare('created_ip',$this->created_ip,true);
+		$criteria->compare('created_date',$this->created_date,true);
+		$criteria->compare('created_user',$this->created_user);
+		$criteria->compare('created_username',$this->created_username,true);
+		$criteria->compare('modified_ip',$this->modified_ip,true);
+		$criteria->compare('modified_date',$this->modified_date,true);
+		$criteria->compare('modified_user',$this->modified_user);
+		$criteria->compare('modified_username',$this->modified_username,true);
+		$criteria->compare('active',$this->active);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return InstructorItem the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ protected/models/TimeboardItem.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * This is the model class for table "timeboard_items".
+ *
+ * The followings are the available columns in table 'timeboard_items':
+ * @property integer $id
+ * @property integer $hall_id
+ * @property integer $instructor_id
+ * @property integer $day_of_week
+ * @property string $time_start
+ * @property string $time_finish
+ * @property string $body
+ * @property string $created_ip
+ * @property string $created_date
+ * @property integer $created_user
+ * @property string $created_username
+ * @property string $modified_ip
+ * @property string $modified_date
+ * @property integer $modified_user
+ * @property string $modified_username
+ * @property integer $active
+ *
+ * The followings are the available model relations:
+ * @property ClubHalls $hall
+ * @property InstructorItems $instructor
+ */
+class TimeboardItem extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'timeboard_items';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('hall_id, day_of_week, time_start, time_finish, body, created_username, modified_username', 'required'),
+			array('hall_id, instructor_id, day_of_week, created_user, modified_user, active', 'numerical', 'integerOnly'=>true),
+			array('body', 'length', 'max'=>500),
+			array('created_ip, modified_ip, modified_username', 'length', 'max'=>300),
+			array('created_username', 'length', 'max'=>200),
+			array('created_date, modified_date', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, hall_id, instructor_id, day_of_week, time_start, time_finish, body, created_ip, created_date, created_user, created_username, modified_ip, modified_date, modified_user, modified_username, active', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'hall' => array(self::BELONGS_TO, 'ClubHalls', 'hall_id'),
+			'instructor' => array(self::BELONGS_TO, 'InstructorItems', 'instructor_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'hall_id' => 'Hall',
+			'instructor_id' => 'Instructor',
+			'day_of_week' => 'Day Of Week',
+			'time_start' => 'Time Start',
+			'time_finish' => 'Time Finish',
+			'body' => 'Body',
+			'created_ip' => 'Created Ip',
+			'created_date' => 'Created Date',
+			'created_user' => 'Created User',
+			'created_username' => 'Created Username',
+			'modified_ip' => 'Modified Ip',
+			'modified_date' => 'Modified Date',
+			'modified_user' => 'Modified User',
+			'modified_username' => 'Modified Username',
+			'active' => 'Active',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('hall_id',$this->hall_id);
+		$criteria->compare('instructor_id',$this->instructor_id);
+		$criteria->compare('day_of_week',$this->day_of_week);
+		$criteria->compare('time_start',$this->time_start,true);
+		$criteria->compare('time_finish',$this->time_finish,true);
+		$criteria->compare('body',$this->body,true);
+		$criteria->compare('created_ip',$this->created_ip,true);
+		$criteria->compare('created_date',$this->created_date,true);
+		$criteria->compare('created_user',$this->created_user);
+		$criteria->compare('created_username',$this->created_username,true);
+		$criteria->compare('modified_ip',$this->modified_ip,true);
+		$criteria->compare('modified_date',$this->modified_date,true);
+		$criteria->compare('modified_user',$this->modified_user);
+		$criteria->compare('modified_username',$this->modified_username,true);
+		$criteria->compare('active',$this->active);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return TimeboardItem the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
