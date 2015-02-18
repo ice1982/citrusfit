193b3772ceede31db628e6904655a0ce8045f777
zzeraw <pekidoch@gmail.com>
2015-02-18 15:16:25 +0300

    Актуализация базового контроллера

--- protected/components/frontend/controllers/FrontEndController.php
+++ protected/components/frontend/controllers/FrontEndController.php
@@ -1,38 +1,11 @@
 <?php
 
-Yii::import('application.modules.blocks.*');
-Yii::import('application.modules.blocks.models.*');
-Yii::import('application.modules.blocks.models._forms.*');
-Yii::import('application.modules.blocks.models._base.*');
-
-Yii::import('application.modules.pages.*');
-Yii::import('application.modules.pages.models.*');
-Yii::import('application.modules.pages.models._forms.*');
-Yii::import('application.modules.pages.models._base.*');
-
-Yii::import('application.modules.forms.*');
-Yii::import('application.modules.forms.models.*');
-Yii::import('application.modules.forms.models._forms.*');
-Yii::import('application.modules.forms.models._base.*');
-Yii::import('application.modules.forms.components.*');
-Yii::import('application.modules.forms.components.custom.*');
-Yii::import('application.modules.forms.components.widgets.*');
-
-Yii::import('application.modules.clubs.*');
-Yii::import('application.modules.clubs.models.*');
-Yii::import('application.modules.clubs.models._forms.*');
-Yii::import('application.modules.clubs.models._base.*');
-
-Yii::import('application.modules.gallery.*');
-Yii::import('application.modules.gallery.models.*');
-Yii::import('application.modules.gallery.models._forms.*');
-Yii::import('application.modules.gallery.models._base.*');
-
 class FrontEndController extends BaseController
 {
     public $layout = '//templates/default';
     public $page;
     public $pageIndex = 1;
+    public $meta_title;
     public $pageDescription;
     public $pageKeywords;
     public $pageTemplate;
@@ -43,26 +16,88 @@ class FrontEndController extends BaseController
 
     public function init()
     {
+        $this->trailingSlashRedirect();
+
+        parent::init();
+
         $this->checkClubSession();
 
         if (isset(Yii::app()->controller->module->page)) {
-            $this->page = Yii::app()->controller->module->page;
+            $page = Yii::app()->controller->module->page;
         }
 
-        // var_dump($this->page);
+        if (isset($page->id)) {
+            $this->page = Yii::app()->controller->module->page;
+
+            $this->setPageMeta($this->page);
+            $this->setPageTemplate($this->page);
+        }
+
+        $this->_generateWidgetsList();
+
 
         if (!empty($this->page->club_id)) {
             Yii::app()->session['club'] = $this->page->club_id;
             $this->checkClubSession();
         }
 
-        // unset($_SESSION);
-        // unset($SESSION);
+        $this->_launchUtmHandler();
 
-        // $club_id = Yii::app()->request->getQuery('club_id');
-        // var_dump($club_id);
+    }
 
-        parent::init();
+    private function _launchUtmHandler()
+    {
+        if (isset($_GET)) {
+            // var_dump($_GET);
+            $json_get_array = json_encode($_GET);
+            // var_dump($json_get_array);
+        }
+        if (!isset(Yii::app()->session['utm_session'])) {
+            if ( (isset($json_get_array)) && (isset($_SERVER['HTTP_REFERER'])) ) {
+                $session = new CHttpSession;
+                $session->open();
+                $session->setTimeout(0);
+
+                $session['utm_session'] = $json_get_array;
+            }
+        } else {
+            // var_dump(Yii::app()->session['utm_session']);
+        }
+    }
+
+    private function _generateWidgetsLocations()
+    {
+        $locations[] = 'application.components.frontend.widgets';
+        $modules = $this->_getModulesNames();
+
+        foreach ($modules as $module) {
+            $locations[] = 'application.modules.' . $module . '.components.widgets';
+        }
+
+        return $locations;
+    }
+
+    private function _generateWidgetsList()
+    {
+        $locations = $this->_generateWidgetsLocations();
+
+        $list = array();
+
+        foreach ($locations as $directory) {
+            $path = Yii::getPathOfAlias($directory);
+
+            if (is_dir($path)) {
+                $files = CFileHelper::findFiles($path, array('fileTypes' => array('php'), 'level' => 0));
+
+                if (count($files)) {
+                    foreach ($files as $file) {
+                        $list[] = basename($file, '.php');
+                    }
+                }
+            }
+        }
+
+        return $list;
     }
 
     public function checkClubSession()
@@ -94,37 +129,12 @@ class FrontEndController extends BaseController
         return array(
             'InlineCommonWidgetsBehavior' => array(
                 'class' => 'MyInlineWidgetsBehavior',
-                'location' => array(
-                    'application.components.frontend.widgets',
-                    'application.modules.forms.components.widgets',
-                ),
+                'location' => $this->_generateWidgetsLocations(),
                 'startBlock' => '{{w:',
                 'endBlock' => '}}',
-                'widgets' => array(
-                    'GalleryBlock',
-                    'CurrentYear',
-                    'HomeUrl',
-                    'MainMenu',
-
-                    'BannersWidget',
-                    'BeginnersWidget',
-                    'NewsBlockWidget',
-                    'AboutClubWidget',
-                    'MapWidget',
-                    'ContactsWidget',
-
-                    'SimpleFormWidget',
-                ),
+                'widgets' => $this->_generateWidgetsList(),
             ),
-            // 'InlineFormWidgetsBehavior' => array(
-            //     'class' => 'DInlineWidgetsBehavior',
-            //     'location' => 'application.modules.forms.components.widgets',
-            //     'startBlock' => '{{w:',
-            //     'endBlock' => '}}',
-            //     'widgets' => array(
-            //         'SimpleFormWidget',
-            //     ),
-            // ),
+
         );
     }
 
@@ -141,28 +151,38 @@ class FrontEndController extends BaseController
 
     protected function setPageMeta($page)
     {
-        $this->pageIndex = $page->meta_index;
-        (empty($page->meta_title)) || $this->setPageTitle($page->meta_title);
-        (empty($page->meta_keywords)) || $this->pageKeywords = $page->meta_keywords;
-        (empty($page->meta_description)) || $this->pageDescription = $page->meta_description;
+        $meta_title = '';
 
-        if ((empty($this->pageKeywords)) || (empty($this->pageDescription))) {
-
-            if (empty($page->alias)) {
-                $homepage = $page;
-            } else {
-                $homepage = Page::model()->findByAlias(null);
-            }
-
-            if ((empty($this->pageKeywords))) {
-                $this->pageKeywords = $homepage->meta_keywords;
-            }
-
-            if ((empty($this->pageDescription))) {
-                $this->pageDescription = $homepage->meta_description;
-            }
+        $homepage = Page::model()->findByAlias('');
+        if (!isset($page->id)) {
+            $page = $homepage;
         }
+
+        if (empty($page->meta_title)) {
+            $meta_title = $homepage->meta_title;
+        } else {
+            $meta_title = $page->meta_title;
+        }
+
+        $meta_description = $homepage->meta_description;
+        $meta_keywords = $homepage->meta_keywords;
+
+        if (empty($meta_title)) {
+            $meta_title = Yii::app()->name;
+        }
+
+        $this->meta_title = $meta_title;
+
+        $meta_description = (!empty($meta_description)) ? $meta_description : $homepage->meta_description;
+        $meta_keywords = (!empty($meta_keywords)) ? $meta_keywords : $homepage->meta_keywords;
+
+        $this->setPageTitle($meta_title);
+
+        $this->pageDescription = $meta_description;
+        $this->pageKeywords = $meta_keywords;
+        $this->pageIndex = $homepage->meta_index;
     }
+
 
     protected function setPageTemplate($page)
     {
@@ -192,6 +212,40 @@ class FrontEndController extends BaseController
             return $model;
         } else {
             throw new CHttpException(404, 'Запрашиваемая страница не найдена.');
+        }
+    }
+
+    protected function trailingSlashRedirect()
+    {
+        if (Yii::app()->request->isAjaxRequest) {
+            return false;
+        }
+
+        // Remove any double slashes and force a trailing slash to the request URI
+
+        $requestUri = yii::app()->request->requestUri;
+
+        if (true === strpos($requestUri, '.')) {
+
+        }
+
+        $repairedRequestUri = $requestUri;
+
+        while (false !== strpos($repairedRequestUri, '//')) {
+            $repairedRequestUri = preg_replace("////", '/', $repairedRequestUri);
+        }
+
+        if (
+            false === strpos($repairedRequestUri, '?') &&
+            '/' !== substr($repairedRequestUri, strlen($repairedRequestUri) - 1, 1)
+        ) {
+            $repairedRequestUri = "{$repairedRequestUri}/";
+        } elseif ('/' !== substr($repairedRequestUri, strpos($repairedRequestUri, '?') - 1, 1)) {
+            $repairedRequestUri = substr($repairedRequestUri, 0, strpos($repairedRequestUri, '?')) . '/' . substr($repairedRequestUri, strpos($repairedRequestUri, '?'));
+        }
+
+        if ( (false === strpos($repairedRequestUri, '.')) && ($repairedRequestUri !== $requestUri) ) {
+            Yii::app()->request->redirect($repairedRequestUri, true, 301);
         }
     }
 
