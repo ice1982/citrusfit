e5de0b93a24210af507e790e1f26275c4a95c17a
ice1982 <ice1982@list.ru>
2016-08-30 18:13:43 +0300

    citrusfit
    мои разработки на сайте

--- app/protected/views/frontend/layouts/main.php
+++ app/protected/views/frontend/layouts/main.php
@@ -26,6 +26,24 @@
 
     <script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
 
+    <!-- Код тега ремаркетинга Google -->
+    <!--------------------------------------------------
+    С помощью тега ремаркетинга запрещается собирать информацию, по которой можно идентифицировать личность пользователя. Также запрещается размещать тег на страницах с контентом деликатного характера. Подробнее об этих требованиях и о настройке тега читайте на странице http://google.com/ads/remarketingsetup.
+    --------------------------------------------------->
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 962541576;
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/962541576/?value=0&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
+
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript">
         (function (d, w, c) {
@@ -76,30 +94,8 @@
         ga('send', 'pageview');
 
     </script>
-
-
-    <!-- Код тега ремаркетинга Google -->
-    <!--------------------------------------------------
-    С помощью тега ремаркетинга запрещается собирать информацию, по которой можно идентифицировать личность пользователя. Также запрещается размещать тег на страницах с контентом деликатного характера. Подробнее об этих требованиях и о настройке тега читайте на странице http://google.com/ads/remarketingsetup.
-    --------------------------------------------------->
-    <script type="text/javascript">
-        /* <![CDATA[ */
-        var google_conversion_id = 962541576;
-        var google_custom_params = window.google_tag_params;
-        var google_remarketing_only = true;
-        /* ]]> */
-    </script>
-    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-    </script>
-    <noscript>
-        <div style="display:inline;">
-            <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/962541576/?value=0&amp;guid=ON&amp;script=0"/>
-        </div>
-    </noscript>
-
-    
-
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <script type="text/javascript" src="/js/swfobject.js"></script>
    <!-- LeadoMed code -->
 <script>
     (function() {
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,49 @@
+
+
 $( document ).ready( function() {
+    g = window;
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+
+    g.onYouTubeIframeAPIReady = function () {
+        g.player = new YT.Player('player', {
+            height: '1000',
+            width: '100%',
+            playerVars: { 'autoplay': 1, 'controls': 0, 'playlist': 'gVuw23C0MYo', 'loop': 1 },
+            videoId: 'gVuw23C0MYo',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    // 4. The API will call this function when the video player is ready.
+    g.onPlayerReady = function(event) {
+        event.target.playVideo();
+        event.target.mute();
+    }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    g.done = false;
+    g.onPlayerStateChange = function(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+            //setTimeout(stopVideo, 6000);
+            done = true;
+        }
+    }
+    g.stopVideo = function () {
+            player.stopVideo();
+        }
 
 
     $('.fancybox-modal').fancybox({
